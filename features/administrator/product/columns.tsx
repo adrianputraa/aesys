e51875d08526f8a6e5bef24217d5fa82e7f78715
adrianputraa <atputrap@gmail.com>
2025-11-16 18:40:43 +0700

    (feat): product table row action

--- features/administrator/product/columns.tsx
+++ features/administrator/product/columns.tsx
@@ -1,7 +1,17 @@
 'use client';
 
 import { ColumnDef } from '@tanstack/react-table';
-
+import { MoreHorizontal } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { toast } from 'sonner';
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
 
@@ -31,6 +41,50 @@ export const columns: ColumnDef<Product>[] = [
       }).format(amount);
 
       return <div className="text-right font-medium">{formatted}</div>;
+    },
+    maxSize: 200,
+  },
+  {
+    id: 'actions',
+    minSize: 40,
+    cell: ({ row }) => {
+      const product = row.original;
+      const url = new URL(`/admin/product/${product.id}`, 'http://localhost:3000');
+
+      const clipboardToast = () => {
+        toast.info('Copied to clipboard', {
+          id: 'copy_to_clipboard',
+        });
+      };
+
+      const copyProductId = () => {
+        navigator.clipboard.writeText(String(product.id));
+        clipboardToast();
+      };
+
+      const copyProductUrl = () => {
+        navigator.clipboard.writeText(url.toString());
+        clipboardToast();
+      };
+
+      return (
+        <div className="flex justify-end">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="ml-auto h-8 w-8 p-0">
+                <MoreHorizontal className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>Actions</DropdownMenuLabel>
+              <DropdownMenuItem onClick={copyProductId}>Copy Product ID</DropdownMenuItem>
+              <DropdownMenuItem onClick={copyProductUrl}>Copy Product URL</DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Edit Product</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      );
     },
   },
 ];
